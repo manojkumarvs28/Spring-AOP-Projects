e55351d764c926a0302dd2b266d1f243f1377470
Manoj Kumar V Sarangmath <manojkumarvs28@gmail.com>
2020-06-12 23:07:33 +0530

    Spring AOP pointcut declarations

---
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,16 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+
+public class MainDemoApp {
+	
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		AccountDAO accountDAO = context.getBean("accountDAO",AccountDAO.class);
+		accountDAO.addAccount();
+		context.close();
+	}
+
+}
